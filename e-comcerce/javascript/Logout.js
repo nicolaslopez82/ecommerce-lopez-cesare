bc0896f71b0fdc72c5eb35cc716617306eeb1665
nicolaslopez82 <nicolaslopez82@gmail.com>
2022-01-31 07:32:02 -0300

    logout - botones e implementacion completado

---
+++ e-comcerce/javascript/Logout.js
@@ -0,0 +1,19 @@
+﻿//WIP
+$(document).on('click', '#idClose', function (e) {
+    e.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "Logout.aspx/CerrarSession",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status + " \n" + xhr.responseText, "\n" + thrownError);
+        },
+        success: function (response) {
+            if (response.d == true) {
+                window.location.href = "Login.aspx";
+            }
+        }
+    });
+});
